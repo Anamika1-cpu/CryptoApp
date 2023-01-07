165a68cd5550af53830e8cebb7fe259fcf28bf15
Anamika1-cpu <anamikagour666@gmailcom>
2023-01-07 21:40:15 +0530

    coin component

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,7 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Radio,
+  RadioGroup,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComp from "./ErrorComp";
+import { Link } from "react-router-dom";
 
 const Coins = () => {
-  return <div>Coins</div>;
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  const changePage = (page) => {
+    setPage(page);
+    setLoading(true);
+  };
+  const btns = new Array(25).fill(1);
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
+        );
+        setCoins(data);
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency, page]);
+  if (error) return <ErrorComp message={"Error while fetching coins"} />;
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <RadioGroup value={currency} onChange={setCurrency}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>₹</Radio>
+              <Radio value={"eur"}>€</Radio>
+              <Radio value={"usd"}>$</Radio>
+            </HStack>
+          </RadioGroup>
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {coins.map((coin) => (
+              <CoinCard
+                key={coin.id}
+                id={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                price={coin.current_price}
+                currencySymbol={currencySymbol}
+              />
+            ))}
+          </HStack>
+          <HStack w={"full"} p={"8"} overflowX={"auto"}>
+            {btns.map((item, index) => (
+              <Button bgColor={"blackAlpha.900"} onClick={() => changePage(2)}>
+                {index + 1}
+              </Button>
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
 };
+
+const CoinCard = ({ name, id, image, symbol, price, currencySymbol }) => (
+  <Link to={`/coins/${id}`}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image src={image} w={"10"} h={"10"} objectFit='cover' alt={"Exchnage"} />
+      <Heading noOfLines={1} size={"md"}>
+        {symbol}
+      </Heading>
+      <Text noOfLines={1}>{name}</Text>
+      <Text noOfLines={1}>{price ? `${currencySymbol}${price}` : "NA"}</Text>
+    </VStack>
+  </Link>
+);
 
 export default Coins;
--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -10,20 +10,28 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import Loader from "./Loader";
+import ErrorComp from "./ErrorComp";
+import { Link } from "react-router-dom";
 // import ExchangeCard from "./ExchangeCard";
 const Exchanges = () => {
   const [exchanges, setExchanges] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchExchanges = async () => {
-      const { data } = await axios.get(`${server}/exchanges`);
-      setExchanges(data);
-      setLoading(false);
-      console.log(data);
+      try {
+        const { data } = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
     };
     fetchExchanges();
   }, []);
-
+  if (error) return <ErrorComp message={"Error while fetching exchanges"} />;
   return (
     <Container maxW={"container.xl"}>
       {loading ? (
@@ -34,6 +42,7 @@ const Exchanges = () => {
             {exchanges.map((exchange) => (
               <ExchangeCard
                 key={exchange.id}
+                id={exchange.id}
                 name={exchange.name}
                 image={exchange.image}
                 rank={exchange.trust_score_rank}
@@ -47,15 +56,27 @@ const Exchanges = () => {
   );
 };
 
-const ExchangeCard = ({ name, image, rank, url }) => (
-  <a href={url} target={"blank"}>
-    <VStack>
+const ExchangeCard = ({ name, id, image, rank, url }) => (
+  <Link to={`/exchanges/${id}`} target={"blank"}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
       <Image src={image} w={"10"} h={"10"} objectFit='cover' alt={"Exchnage"} />
       <Heading noOfLines={1} size={"md"}>
         {rank}
       </Heading>
       <Text noOfLines={1}>{name}</Text>
     </VStack>
-  </a>
+  </Link>
 );
 export default Exchanges;
